8d2341c45942c672e6fe5c0a4f49e5579cd01930
amar <amarmann92@gmail.com>
2021-03-18 17:23:29 -0700

    routing in place

---
+++ routes/apiroutes.js
@@ -0,0 +1,64 @@
+const router = require('express').Router();
+const db = require('../models/db.js');
+
+router.post('/api/workouts', (req, res) => {
+  db.create({})
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.put('/api/workouts/:id', (req,res) => {
+  db.findByIdAndUpdate(
+    req.params.id,
+    { $push: { exercises: req.body } },
+  )
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get('/api/workouts', (req, res) => {
+  db.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .then((data) => {
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
+
+router.get('/api/workouts/range', (req, res) => {
+  db.aggregate([
+    {
+      $addFields: {
+        totalDuration: {
+          $sum: '$exercises.duration',
+        },
+      },
+    },
+  ])
+    .sort({ _id: -1 })
+    .limit(7)
+    .then((data) => {
+      console.log(data);
+      res.json(data);
+    })
+    .catch((err) => {
+      res.json(err);
+    });
+});
